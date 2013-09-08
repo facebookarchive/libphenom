deaa86f9a4070ac49b2db884fd952d882ec1a157
Wez Furlong <wez@fb.com>
2013-09-08 09:48:24 -0700

    Add buffer API

---
+++ corelib/buf.c
@@ -0,0 +1,687 @@
+/*
+ * Copyright 2013 Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "phenom/buffer.h"
+#include "phenom/memory.h"
+#include "phenom/refcnt.h"
+#include "phenom/queue.h"
+#include "phenom/log.h"
+
+struct ph_buf {
+  ph_refcnt_t ref;
+  ph_buf_t *slice;
+  uint8_t *buf;
+  uint64_t size;
+  ph_memtype_t memtype;
+};
+
+struct ph_bufq_ent {
+  PH_STAILQ_ENTRY(ph_bufq_ent) ent;
+  ph_buf_t *buf;
+  // Offset into the buf of the data that is yet to be consumed
+  uint64_t rpos;
+  // Offset at which to append further data
+  uint64_t wpos;
+};
+
+struct ph_bufq {
+  PH_STAILQ_HEAD(bufqhead, ph_bufq_ent) fifo;
+  // Maximum amount of storage to allow
+  uint64_t max_size;
+};
+
+static ph_memtype_def_t defs[] = {
+  { "buffer", "object", sizeof(ph_buf_t), PH_MEM_FLAGS_ZERO },
+  { "buffer", "8k", 8*1024, 0 },
+  { "buffer", "16k", 16*1024, 0 },
+  { "buffer", "32k", 32*1024, 0 },
+  { "buffer", "64k", 64*1024, 0 },
+  { "buffer", "vsize", 0, 0 },
+  { "buffer", "queue", sizeof(ph_bufq_t), PH_MEM_FLAGS_ZERO },
+  { "buffer", "queue_ent", sizeof(struct ph_bufq_ent), PH_MEM_FLAGS_ZERO },
+};
+
+static struct {
+  ph_memtype_t obj, f8k, f16k, f32k, f64k, vsize, queue, queue_ent;
+} mt;
+
+static pthread_once_t once_init = PTHREAD_ONCE_INIT;
+
+static void buffer_init(void)
+{
+  ph_memtype_register_block(sizeof(defs)/sizeof(defs[0]), defs, &mt.obj);
+}
+
+static inline uint64_t select_size(uint64_t size, ph_memtype_t *mtp)
+{
+  ph_memtype_t t;
+
+  if (size < 8192) {
+    t = mt.f8k;
+    size = 8192;
+  } else if (size < 16 * 1024) {
+    t = mt.f16k;
+    size = 16 * 1024;
+  } else if (size < 32 * 1024) {
+    t = mt.f32k;
+    size = 32 * 1024;
+  } else if (size < 64 * 1024) {
+    t = mt.f64k;
+    size = 64 * 1024;
+  } else {
+    t = mt.vsize;
+    size = 0;
+  }
+
+  if (mtp) {
+    *mtp = t;
+  }
+  return size;
+}
+
+ph_buf_t *ph_buf_new(uint64_t size)
+{
+  ph_buf_t *buf;
+  uint64_t selected;
+
+  pthread_once(&once_init, buffer_init);
+  buf = ph_mem_alloc(mt.obj);
+
+  if (!buf) {
+    return NULL;
+  }
+
+  /* Find the right size bucket.
+   * We report their requested size, even if the bucket we pick is
+   * bigger, so that we can sanely handle the case when they copy
+   * memory of a known size into a buffer.
+   * If they requested a 0 size, we report the bucket size as this
+   * pattern is asking for some buffer space and they don't care
+   * how big it is, so long as they know how big it is. */
+  buf->size = size;
+  selected = select_size(size, &buf->memtype);
+  if (size == 0) {
+    buf->size = selected;
+  }
+
+  if (buf->memtype == mt.vsize) {
+    buf->buf = ph_mem_alloc_size(buf->memtype, size);
+    if (!buf->buf) {
+      ph_mem_free(mt.obj, buf);
+      return NULL;
+    }
+  }
+
+  if (!buf->buf) {
+    buf->buf = ph_mem_alloc(buf->memtype);
+    if (!buf->buf) {
+      ph_mem_free(mt.obj, buf);
+      return NULL;
+    }
+  }
+
+  buf->ref = 1;
+  return buf;
+}
+
+void ph_buf_addref(ph_buf_t *buf)
+{
+  ph_refcnt_add(&buf->ref);
+}
+
+void ph_buf_delref(ph_buf_t *buf)
+{
+  if (!ph_refcnt_del(&buf->ref)) {
+    return;
+  }
+
+  if (buf->slice) {
+    ph_buf_delref(buf->slice);
+  } else {
+    ph_mem_free(buf->memtype, buf->buf);
+  }
+  ph_mem_free(mt.obj, buf);
+}
+
+ph_buf_t *ph_buf_slice(ph_buf_t *buf, uint64_t start, uint64_t len)
+{
+  ph_buf_t *slice;
+
+  if (len == 0) {
+    if (start >= buf->size) {
+      return NULL;
+    }
+    len = buf->size - start;
+  }
+
+  if (start + len > buf->size) {
+    return NULL;
+  }
+
+  if (start == 0 && len == buf->size) {
+    // Just return another ref on this one
+    ph_buf_addref(buf);
+    return buf;
+  }
+
+  slice = ph_mem_alloc(mt.obj);
+  if (!slice) {
+    return NULL;
+  }
+
+  slice->slice = buf;
+  ph_buf_addref(buf);
+  slice->buf = buf->buf + start;
+  slice->size = len;
+  slice->ref = 1;
+
+  return slice;
+}
+
+bool ph_buf_copy_mem(ph_buf_t *dest, void *mem, uint64_t len, uint64_t dest_start)
+{
+  if (dest_start + len > dest->size) {
+    return false;
+  }
+
+  memmove(dest->buf + dest_start, mem, len);
+  return true;
+}
+
+bool ph_buf_copy(ph_buf_t *src, ph_buf_t *dest, uint64_t start, uint64_t len,
+    uint64_t dest_start)
+{
+  if (start + len > src->size) {
+    return false;
+  }
+  if (dest_start + len > dest->size) {
+    return false;
+  }
+  memmove(dest->buf + dest_start, src->buf + start, len);
+  return true;
+}
+
+bool ph_buf_set(ph_buf_t *buf, int value, uint64_t start, uint64_t len)
+{
+  if (start + len > buf->size) {
+    return false;
+  }
+  memset(buf->buf + start, value, len);
+  return true;
+}
+
+uint64_t ph_buf_len(ph_buf_t *buf)
+{
+  return buf->size;
+}
+
+uint8_t *ph_buf_mem(ph_buf_t *buf)
+{
+  return buf->buf;
+}
+
+ph_buf_t *ph_buf_concat(uint64_t total_length, uint32_t num_buffers,
+    ph_buf_t **buffers, uint64_t first_offset)
+{
+  uint32_t i;
+  uint64_t off;
+  ph_buf_t *buf;
+
+  if (total_length == 0) {
+    for (i = 0; i < num_buffers; i++) {
+      total_length += buffers[i]->size;
+    }
+    total_length -= first_offset;
+  }
+
+  buf = ph_buf_new(total_length);
+  if (!buf) {
+    return NULL;
+  }
+
+  for (off = 0, i = 0; off < total_length && i < num_buffers; i++) {
+    uint64_t len = buffers[i]->size;
+    if (off + len > total_length) {
+      len = total_length - off;
+    }
+    if (!ph_buf_copy(buffers[i], buf,
+        i == 0 ? first_offset : 0,
+        len, off)) {
+      ph_buf_delref(buf);
+      return NULL;
+    }
+    off += buffers[i]->size;
+  }
+
+  return buf;
+}
+
+static struct ph_bufq_ent *q_add_new_buf(ph_bufq_t *q, uint64_t bufsize)
+{
+  struct ph_bufq_ent *ent;
+
+  ent = ph_mem_alloc(mt.queue_ent);
+  if (!ent) {
+    return NULL;
+  }
+
+  ent->buf = ph_buf_new(bufsize);
+  if (!ent->buf) {
+    ph_mem_free(mt.queue_ent, ent);
+    return NULL;
+  }
+
+  PH_STAILQ_INSERT_TAIL(&q->fifo, ent, ent);
+
+  return ent;
+}
+
+ph_bufq_t *ph_bufq_new(uint64_t max_size)
+{
+  ph_bufq_t *q;
+
+  pthread_once(&once_init, buffer_init);
+  q = ph_mem_alloc(mt.queue);
+  if (!q) {
+    return NULL;
+  }
+
+  PH_STAILQ_INIT(&q->fifo);
+  q->max_size = max_size;
+
+  if (!q_add_new_buf(q, 0)) {
+    ph_mem_free(mt.queue, q);
+    return NULL;
+  }
+
+  return q;
+}
+
+ph_result_t ph_bufq_append(ph_bufq_t *q, void *buf, uint64_t len,
+    uint64_t *added_bytes)
+{
+  struct ph_bufq_ent *last, *append;
+  char *cbuf = buf;
+  uint64_t avail, buflen;
+
+  last = PH_STAILQ_LAST(&q->fifo, ph_bufq_ent, ent);
+  avail = ph_buf_len(last->buf) - last->wpos;
+
+  // Top-off
+  if (avail) {
+    ph_buf_copy_mem(last->buf, cbuf, avail, last->wpos);
+    last->wpos += avail;
+    len -= avail;
+    cbuf += avail;
+  }
+
+  if (len == 0) {
+    if (added_bytes) {
+      *added_bytes = avail;
+    }
+    return PH_OK;
+  }
+
+  // We need more storage
+  buflen = select_size(len, NULL);
+  append = q_add_new_buf(q, buflen);
+
+  if (!append) {
+    // Roll back!
+    last->wpos -= avail;
+    return PH_NOMEM;
+  }
+
+  ph_buf_copy_mem(append->buf, cbuf, len, 0);
+  append->wpos = len;
+
+  if (added_bytes) {
+    *added_bytes = avail + len;
+  }
+
+  return PH_OK;
+}
+
+static uint64_t bufq_len(ph_bufq_t *q, uint32_t *num_ents)
+{
+  uint64_t avail = 0;
+  struct ph_bufq_ent *ent;
+  uint32_t n = 0;
+
+  PH_STAILQ_FOREACH(ent, &q->fifo, ent) {
+    avail += ent->wpos - ent->rpos;
+    n++;
+  }
+
+  if (num_ents) {
+    *num_ents = n;
+  }
+
+  return avail;
+}
+
+uint64_t ph_bufq_len(ph_bufq_t *q)
+{
+  return bufq_len(q, NULL);
+}
+
+static void gc_bufq(ph_bufq_t *q)
+{
+  struct ph_bufq_ent *ent, *last;
+
+  last = PH_STAILQ_LAST(&q->fifo, ph_bufq_ent, ent);
+  while (true) {
+    ent = PH_STAILQ_FIRST(&q->fifo);
+    if (!ent) {
+      // Nothing to prune
+      break;
+    }
+
+    if (ent->rpos != ent->wpos) {
+      // Data to be consumed: can't prune it
+      break;
+    }
+
+    if (ent == last && ent->wpos < ph_buf_len(ent->buf)) {
+      // It has room for some appends
+      break;
+    }
+
+    // We don't need this one
+    PH_STAILQ_REMOVE_HEAD(&q->fifo, ent);
+    ph_buf_delref(ent->buf);
+    ph_mem_free(mt.queue_ent, ent);
+  }
+}
+
+static ph_buf_t *slice_bufq(ph_bufq_t *q, uint64_t len, bool consume)
+{
+  struct ph_bufq_ent *ent;
+  uint64_t next;
+  uint32_t n;
+  ph_buf_t *buf = NULL;
+
+  if (len == 0 || len > bufq_len(q, &n)) {
+    return NULL;
+  }
+
+  if (n == 1) {
+    // We can simply slice the first one
+    ent = PH_STAILQ_FIRST(&q->fifo);
+    buf = ph_buf_slice(ent->buf, ent->rpos, len);
+
+    if (!buf) {
+      return NULL;
+    }
+
+    if (consume) {
+      ent->rpos += len;
+      gc_bufq(q);
+    }
+
+    return buf;
+  }
+
+  buf = ph_buf_new(len);
+  if (!buf) {
+    return NULL;
+  }
+
+  next = 0;
+  PH_STAILQ_FOREACH(ent, &q->fifo, ent) {
+    ph_buf_copy(ent->buf, buf, ent->rpos, ent->wpos - ent->rpos, next);
+    next += ent->wpos - ent->rpos;
+    if (consume) {
+      ent->rpos = ent->wpos;
+    }
+  }
+
+  if (consume) {
+    gc_bufq(q);
+  }
+
+  return buf;
+}
+
+ph_buf_t *ph_bufq_consume_bytes(ph_bufq_t *q, uint64_t len)
+{
+  return slice_bufq(q, len, true);
+}
+
+ph_buf_t *ph_bufq_peek_bytes(ph_bufq_t *q, uint64_t len)
+{
+  return slice_bufq(q, len, false);
+}
+
+// If the end of ent->buf is occupied by a prefix string of delim, return
+// the number of suffix bytes that we need to search into the next ent
+static uint32_t partial_match(struct ph_bufq_ent *ent, const char *delim,
+    uint32_t delim_len)
+{
+  uint32_t slen;
+  uint8_t *bend = ph_buf_mem(ent->buf) + ent->wpos;
+  uint8_t *bstart = ph_buf_mem(ent->buf) + ent->rpos;
+
+  for (slen = delim_len - 1; slen > 0; slen--) {
+    uint8_t *buf = bend - slen;
+    if (buf < bstart) {
+      continue;
+    }
+
+    if (memcmp(buf, delim, slen) == 0) {
+      return delim_len - slen;
+    }
+  }
+
+  return 0;
+}
+
+// Locate the record.
+// We look for the delimiter text; if we find it in the buffer queue, we return
+// the length from the start of the queue to the end of the delimiter.
+// Our caller can then pass this length to consume_bytes or peek_bytes to obtain
+// the appropriate slice.
+// Returns 0 if the delimiter was not found
+static uint64_t find_record(ph_bufq_t *q, const char *delim, uint32_t delim_len)
+{
+  struct ph_bufq_ent *ent, *next;
+  uint64_t searched = 0;
+  uint32_t slen;
+
+  PH_STAILQ_FOREACH(ent, &q->fifo, ent) {
+    uint8_t *bstart = ph_buf_mem(ent->buf) + ent->rpos;
+    uint64_t len = ent->wpos - ent->rpos;
+    uint8_t *found;
+
+    found = memmem(bstart, len, delim, delim_len);
+
+    if (found) {
+      searched += (found + delim_len) - bstart;
+      return searched;
+    }
+
+    next = PH_STAILQ_NEXT(ent, ent);
+    if (next == NULL) {
+      // Not found
+      return 0;
+    }
+
+    // check straddle
+    slen = partial_match(ent, delim, delim_len);
+    if (slen) {
+      uint32_t dlen = delim_len - slen;
+
+      bstart = ph_buf_mem(next->buf) + next->rpos;
+      len = next->wpos - next->rpos;
+
+      if (len < slen) {
+        // Not found, nor findable.
+        // Consider this case:
+        // |8192|1|8192|
+        // Where the numbers represent ents pointing to buffers of the indicated
+        // size.  If our delimiter len is 3, and the delimiter straddles three
+        // entries, we won't ever find it.
+        // We don't construct such a sequence today, but if we were to allow a
+        // "zero copy" interface where buffers could be directly chained in, this
+        // is something that might come up.  Whoever adds that can solve that.
+        return 0;
+      }
+
+      if (memcmp(bstart, delim + dlen, slen) == 0) {
+        // Found it
+        searched += len + slen;
+        return searched;
+      }
+    }
+
+    searched += len;
+  }
+
+  // Not found
+  return 0;
+}
+
+ph_buf_t *ph_bufq_consume_record(ph_bufq_t *q, const char *delim, uint32_t delim_len)
+{
+  uint64_t len;
+
+  len = find_record(q, delim, delim_len);
+  if (len == 0) {
+    return NULL;
+  }
+
+  return ph_bufq_consume_bytes(q, len);
+}
+
+ph_buf_t *ph_bufq_peek_record(ph_bufq_t *q, const char *delim, uint32_t delim_len)
+{
+  uint64_t len;
+
+  len = find_record(q, delim, delim_len);
+  if (len == 0) {
+    return NULL;
+  }
+
+  return ph_bufq_peek_bytes(q, len);
+}
+
+bool ph_bufq_stm_write(ph_bufq_t *q, ph_stream_t *stm, uint64_t *nwrotep)
+{
+  struct iovec iov[16];
+  uint32_t nio = 0;
+  struct ph_bufq_ent *ent;
+  uint64_t nwrote;
+  bool res;
+
+  PH_STAILQ_FOREACH(ent, &q->fifo, ent) {
+    uint8_t *buf = ph_buf_mem(ent->buf) + ent->rpos;
+    uint64_t len = ent->wpos - ent->rpos;
+
+    if (nio >= sizeof(iov)/sizeof(iov[0])) {
+      break;
+    }
+
+    if (len == 0) {
+      // Shouldn't happen, but maybe we haven't gc'd?
+      continue;
+    }
+
+    iov[nio].iov_base = buf;
+    iov[nio].iov_len = len;
+    nio++;
+  }
+
+  if (nio == 0) {
+    *nwrotep = 0;
+    return true;
+  }
+
+  res = ph_stm_writev(stm, iov, nio, &nwrote);
+
+  if (nwrotep) {
+    *nwrotep = nwrote;
+  }
+
+  if (res && nwrote > 0) {
+    // Mark them as being consumed
+    PH_STAILQ_FOREACH(ent, &q->fifo, ent) {
+      uint64_t len = ent->wpos - ent->rpos;
+
+      ent->rpos = ent->wpos;
+      nwrote -= len;
+      if (nwrote == 0) {
+        break;
+      }
+    }
+    gc_bufq(q);
+  }
+
+  return res;
+}
+
+bool ph_bufq_stm_read(ph_bufq_t *q, ph_stream_t *stm, uint64_t *nreadp)
+{
+  struct ph_bufq_ent *last;
+  uint64_t avail, nread;
+  bool res;
+
+  last = PH_STAILQ_LAST(&q->fifo, ph_bufq_ent, ent);
+  avail = ph_buf_len(last->buf) - last->wpos;
+
+  if (!avail) {
+    last = q_add_new_buf(q, 0);
+    if (!last) {
+      return false;
+    }
+    avail = ph_buf_len(last->buf);
+  }
+
+  res = ph_stm_read(stm, ph_buf_mem(last->buf) + last->wpos, avail, &nread);
+  if (res) {
+    last->wpos += nread;
+  }
+
+  if (nreadp) {
+    *nreadp = nread;
+  }
+
+  return res;
+}
+
+void ph_bufq_free(ph_bufq_t *q)
+{
+  struct ph_bufq_ent *ent;
+
+  while (true) {
+    ent = PH_STAILQ_FIRST(&q->fifo);
+    if (!ent) {
+      // Nothing to prune
+      break;
+    }
+
+    // We don't need this one
+    PH_STAILQ_REMOVE_HEAD(&q->fifo, ent);
+    ph_buf_delref(ent->buf);
+    ph_mem_free(mt.queue_ent, ent);
+  }
+
+  ph_mem_free(mt.queue, q);
+}
+
+
+/* vim:ts=2:sw=2:et:
+ */
+
---
+++ include/phenom/buffer.h
@@ -0,0 +1,276 @@
+/*
+ * Copyright 2013 Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef PHENOM_BUFFER_H
+#define PHENOM_BUFFER_H
+
+/**
+ * # Buffers
+ *
+ * The Phenom buffer API allows for fixed-size buffers to be used reasonably
+ * efficiently in an application.  The API provides a couple of benefits:
+ *
+ * * Slices can be made over a region of another buffer.  The slice allows
+ *   for the region to be referenced without copying, while still safely
+ *   managing the underlying storage
+ * * The object representation of a string buffer means that we can avoid
+ *   string interning costs when integrating with runtimes such as Lua
+ * * Discontiguous buffers can be used to accumulate data.  The fixed size
+ *   of these buffers helps to reduce heap fragmentation.  An API is provided
+ *   to locate and slice (or duplicate if needed) a delimited record (such
+ *   as CRLF delimited lines)
+ */
+
+#include "phenom/defs.h"
+#include "phenom/stream.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct ph_buf;
+typedef struct ph_buf ph_buf_t;
+
+struct ph_bufq;
+typedef struct ph_bufq ph_bufq_t;
+
+/** Create a new buffer
+ *
+ * The buffer will be at least the size that you requested; the implementation
+ * may decide to round your allocation up to one of a set of common buffer
+ * sizes.
+ *
+ * If you specify 0 for the size, a default buffer size will be selected
+ * for you.
+ *
+ * The contents of the buffer are undefined.
+ *
+ * Returns a reference to the buffer.  When the final reference is released,
+ * the storage is reclaimed.
+ *
+ * Use ph_buf_delref() to release a reference to a buffer.
+ */
+ph_buf_t *ph_buf_new(uint64_t size);
+
+/** Add a reference to a buffer */
+void ph_buf_addref(ph_buf_t *buf);
+
+/** Release a reference to a buffer */
+void ph_buf_delref(ph_buf_t *buf);
+
+/** Create a slice over a buffer
+ *
+ * Creates a new buffer object that references a region in another buffer.
+ *
+ * The slice takes a reference on the other buffer; when the slice is
+ * destroyed, it will release that reference.
+ *
+ * Operating on the slice operates on the specified region of the original
+ * buffer.
+ *
+ * If len == 0, len is interpreted as the length that covers the region from
+ * the start to the end of the buffer.
+ */
+ph_buf_t *ph_buf_slice(ph_buf_t *buf, uint64_t start, uint64_t len);
+
+/** Copy data from one buffer to another
+ *
+ * Copies the specified range of bytes from the source buffer to the
+ * destination buffer at the specified offset.
+ *
+ * The source and destination buffer may be the same object; the memory
+ * ranges may overlap.
+ *
+ * If either the source or destination range is out of bounds, no copying
+ * takes place and this function will return false.
+ *
+ * Otherwise, returns true
+ */
+bool ph_buf_copy(ph_buf_t *src, ph_buf_t *dest, uint64_t start, uint64_t len,
+    uint64_t dest_start);
+
+bool ph_buf_copy_mem(ph_buf_t *dest, void *mem, uint64_t len, uint64_t dest_start);
+
+/** Fills a buffer region with a byte value
+ *
+ * Logically equivalent to memset(3), but operates on a range in a buffer
+ */
+bool ph_buf_set(ph_buf_t *buf, int value, uint64_t start, uint64_t len);
+
+/** Returns the size of the buffer
+ *
+ * Return the number of bytes of available storage in the buffer; this is
+ * the size of the allocated storage space.  The buffer API doesn't track
+ * whether bytes are "used" (there is no cursor, since slices make this
+ * awkward to track), so this is simply the raw number of bytes in the
+ * memory backing the buffer object.
+ */
+uint64_t ph_buf_len(ph_buf_t *buf);
+
+/** Returns a pointer to the start of the buffer memory
+ *
+ * This is the raw buffer memory; you are responsible for constraining your
+ * read/writes to this region within the length specified by ph_buf_len().
+ */
+uint8_t *ph_buf_mem(ph_buf_t *buf);
+
+/** Concatenate a sequence of buffers together
+ *
+ * Allocates a buffer that is large enough to hold the total number of
+ * bytes specified, then copies data from the sequence of buffers into
+ * this new buffer.
+ *
+ * This is useful when matching a record across a series of buffers and
+ * then wanting to pass the data to code that requires a contiguous memory
+ * buffer.
+ *
+ * If length == 0, the function will query the total length of the buffers
+ * and use that as the length.
+ *
+ * `first_offset` specifies the number of bytes to skip in the first buffer.
+ */
+ph_buf_t *ph_buf_concat(uint64_t length, uint32_t num_bufs,
+    ph_buf_t **buffers, uint64_t first_offset);
+
+/** Create a new buffer queue
+ *
+ * A buffer queue allows buffering of buffers in a FIFO manner.
+ * This is useful to implement a segmented read or write buffer with an
+ * upper bound on buffer size.
+ *
+ * A freshly created buffer queued is populated with an initial ph_buf_t
+ * with the default size.
+ */
+ph_bufq_t *ph_bufq_new(uint64_t max_size);
+
+/** Destroy a buffer queue
+ *
+ * Releases all of its resources
+ */
+void ph_bufq_free(ph_bufq_t *q);
+
+/** Accumulate data into a buffer queue
+ *
+ * Copies the data from the provided raw buffer pointer into the buffer
+ * queue.  Attempts to top off the "last" logical ph_buf_t in the queue.
+ * If the data being added won't fit in the buffer, a new buffer is allocated
+ * and appended.
+ *
+ * If the data to be appended would exceed the maximum size of the buffer
+ * queue, only the data up to the size limit will be added.
+ *
+ * Returns PH_OK on success (which may mean partial success), or an error
+ * code on failure (such as failure to allocate buffers).
+ *
+ * Populates the number of bytes that were consumed in the added_bytes
+ * parameter.
+ */
+ph_result_t ph_bufq_append(ph_bufq_t *q, void *buf, uint64_t len,
+    uint64_t *added_bytes);
+
+/** Attempts to de-queue data from a buffer queue
+ *
+ * If the requested number of bytes are available in the queue, concatenate
+ * them into a contiguous buffer and return that buffer.
+ *
+ * Those bytes are considered read and the queue will
+ * release its reference on the associated buffers, potentially releasing
+ * memory if those references fall to zero.
+ *
+ * If the requested number of bytes are not present, returns NULL.
+ */
+ph_buf_t *ph_bufq_consume_bytes(ph_bufq_t *q, uint64_t len);
+
+/** Peek at data in the buffer queue
+ *
+ * If the requested number of bytes are available in the queue, concatenate
+ * them into a contiguous buffer and return that buffer.
+ *
+ * The state of the queue is left unchanged.
+ *
+ * If the requested number of bytes are not present, returns NULL.
+ */
+ph_buf_t *ph_bufq_peek_bytes(ph_bufq_t *q, uint64_t len);
+
+/** Attempts to de-queue a record from a buffer queue
+ *
+ * Searches the buffer queue until it finds the delimiter text.
+ * If the delimiter text is not found, returns NULL.
+ *
+ * If the delimiter text is found, constructs a buffer to reference
+ * that memory region and dequeues it as though ph_bufq_consume_bytes()
+ * was called with the appropriate length to the end of the delimiter text.
+ *
+ * The delimiter is included in the returned buffer.
+ */
+ph_buf_t *ph_bufq_consume_record(ph_bufq_t *q, const char *delim, uint32_t delim_len);
+
+/** Attempts to peek at a record from a buffer queue
+ *
+ * Searches the buffer queue until it finds the delimiter text.
+ * If the delimiter text is not found, returns NULL.
+ *
+ * If the delimiter text is found, constructs a buffer to reference
+ * that memory region and peeks at it as though ph_bufq_peek_bytes()
+ * was called with the appropriate length to the end of the delimiter text.
+ *
+ * The delimiter is included in the returned buffer.
+ */
+ph_buf_t *ph_bufq_peek_record(ph_bufq_t *q, const char *delim, uint32_t delim_len);
+
+/** Attempts to consume data data from a queue and write to a stream
+ *
+ * Constructs an iovec representing the queued buffers up to an internal
+ * limit of iovec elements, then calls the writev implementation of the stream,
+ * ignoring any buffering that may be present on the stream.  It is recommended
+ * that you use an unbuffered stream, but if you decide not to, you are responsible
+ * for ensuring that the stream has been flushed prior to calling this function.
+ *
+ * Any successfully written bytes are considered consumed and the associated
+ * storage may be released.
+ *
+ * Returns true on success (which will likely be a partial write) and sets
+ * *nwrote to the number of bytes that were consumed.
+ *
+ * Returns false on failure; you may call ph_stm_errno() to determine what
+ * went wrong.
+ */
+bool ph_bufq_stm_write(ph_bufq_t *q, ph_stream_t *stm, uint64_t *nwrote);
+
+/** Attempts to read data from a stream and accumulate it in bufq
+ *
+ * If the bufq has a partially filled buffer at the tail, ph_stm_read() will
+ * be invoked to attempt to fill that space.
+ *
+ * If not bufs with space are present in the bufq, a default buffer will be
+ * added and ph_stm_read() will be invoked to fill that space.
+ *
+ * This function will invoke ph_stm_read() at most once per call.
+ */
+bool ph_bufq_stm_read(ph_bufq_t *q, ph_stream_t *stm, uint64_t *nread);
+
+/** Returns the number of bytes to be consumed */
+uint64_t ph_bufq_len(ph_bufq_t *q);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
+/* vim:ts=2:sw=2:et:
+ */
+
