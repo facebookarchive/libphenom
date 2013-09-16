4840489f1691e1232e065507985097a5feec7780
Wez Furlong <wez@fb.com>
2013-09-16 14:49:08 -0700

    kick travis

--- travis/deps.sh
+++ travis/deps.sh
@@ -2,10 +2,8 @@
 set -x
 case `uname` in
   Linux)
-    sudo sysctl -A | grep inotify
     sudo apt-get update -y -q
     sudo apt-get install -y valgrind -q
     sudo apt-get install -y libc-ares-dev -q
     ;;
 esac
-set -e
