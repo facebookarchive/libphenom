5c59fbba544c3b4c841851e6f0788abe7968c436
Wez Furlong <wez@fb.com>
2013-10-10 09:22:09 -0700

    [docs] fixup parameter names
    resolves https://github.com/facebook/libphenom/issues/36

--- include/phenom/counter.h
+++ include/phenom/counter.h
@@ -197,7 +197,7 @@ bool ph_counter_scope_register_counter_block(
  * Note that you may add a negative counter value to decrement it.
  *
  * * `scope` - the scope of the counter
- * * `slot` - the counter slot offset
+ * * `offset` - the counter slot offset
  * * `value` - the value to add to the current counter value.
  */
 void ph_counter_scope_add(
@@ -255,7 +255,7 @@ static inline void ph_counter_block_record_write(
  * Note that you may add a negative counter value to decrement it.
  *
  * * `block` - the block containing the counters
- * * `slot` - the counter slot offset
+ * * `offset` - the counter slot offset
  * * `value` - the value to add to the current counter value.
  */
 static inline void ph_counter_block_add(
@@ -320,7 +320,7 @@ static inline void ph_counter_block_bulk_add(
  * If the offset is invalid, returns 0.
  *
  * * `scope` - the containing scope
- * * `slot` - the counter slot offset
+ * * `offset` - the counter slot offset
  */
 int64_t ph_counter_scope_get(
     ph_counter_scope_t *scope,
