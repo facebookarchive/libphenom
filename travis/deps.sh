6e88a904cf0fae84d2d0c81ca123a4fc9702f3ae
Wez Furlong <wez@fb.com>
2014-04-19 09:08:18 -0700

    Fix up travis build for unbundled CK
    Since we don't bundle the library, we need to fetch and build it as part
    of the travis run.

--- travis/deps.sh
+++ travis/deps.sh
@@ -7,3 +7,18 @@ case `uname` in
     sudo apt-get install -y libc-ares-dev -q
     ;;
 esac
+
+HERE=$PWD
+
+# There is no CK package available for ubuntu yet
+CK_VER=0.3.2
+CK_RELEASE=https://github.com/sbahra/ck/archive/$CK_VER.tar.gz
+if test ! -f $CK_VER.tar.gz ; then
+  wget $CK_RELEASE
+  tar xzf $CK_VER.tar.gz
+fi
+cd ck-$CK_VER
+./configure --prefix=$HERE/thirdparty/ck || exit 1
+make install || exit 1
+cd ..
+
--- travis/run.sh
+++ travis/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -x
 ./autogen.sh
-./configure --enable-address-sanitizer --with-cares
+PKG_CONFIG_PATH=$PWD/thirdparty/ck/lib/pkgconfig ./configure --enable-address-sanitizer --with-cares
 make -j
 make -j clang-analyze
 make check
