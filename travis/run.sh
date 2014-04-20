93f9015d2aaa1a7253bbbd3e9fa9c2aa8a3c02e6
Wez Furlong <wez@fb.com>
2014-04-20 13:12:19 -0700

    Fixup install rule for ck unbundling
    Also add `make install` to travis to try to catch this sort of issue
    faster in the future.

--- travis/run.sh
+++ travis/run.sh
@@ -5,3 +5,6 @@ PKG_CONFIG_PATH=$PWD/thirdparty/ck/lib/pkgconfig ./configure --enable-address-sa
 make -j
 make -j clang-analyze
 make check
+INST_TEST=/tmp/phenom-install-test
+test -d $INST_TEST && rm -rf $INST_TEST
+make DESTDIR=$INST_TEST install
